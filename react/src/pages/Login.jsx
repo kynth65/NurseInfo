e57a1e4641f472b044e3c6c7449deab8c96e2a6e
kynth65 <kynth65@gmail.com>
2025-07-23 17:14:24 +0800

    update login ui

--- react/src/pages/Login.jsx
+++ react/src/pages/Login.jsx
@@ -136,22 +136,7 @@ export default function Login() {
                                     </div>
                                 </div>
 
-                                <div className="flex items-center justify-between">
-                                    <div className="flex items-center">
-                                        <input
-                                            id="remember_me"
-                                            name="remember_me"
-                                            type="checkbox"
-                                            className="h-4 w-4 text-purple-600 focus:ring-purple-500 border-gray-300 rounded"
-                                        />
-                                        <label
-                                            htmlFor="remember_me"
-                                            className="ml-2 block text-sm text-gray-700"
-                                        >
-                                            Remember me
-                                        </label>
-                                    </div>
-
+                                <div className="flex items-center justify-center">
                                     <div className="text-sm">
                                         <a
                                             href="#"
